4016a25d0e451e95aba4837173a9f938a71c5e01
Ryan W <rbrickproductions123@gmail.com>
2026-03-15 17:11:38 -0500

    chore: define config

--- rig/config/config.go
+++ rig/config/config.go
@@ -1,7 +1,26 @@
 package config
 
+import _ "embed"
+
+//go:embed defaults.yaml
+var DefaultConfig string
+
 type NINAConfig struct {
 	Host string `yaml:"host"`
 }
 
-type Config struct{}
+type AdapterConfig struct {
+	Type string `yaml:"type"`
+}
+
+type AuthConfig struct {
+	Token string `yaml:"token"`
+}
+
+type Config struct {
+	ID          string        `yaml:"id"`
+	DisplayName string        `yaml:"display_name"`
+	NINA        NINAConfig    `yaml:"nina,omitempty"`
+	Adapter     AdapterConfig `yaml:"adapter"`
+	Auth        AuthConfig    `yaml:"auth,omitempty"`
+}
